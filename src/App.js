37b4be29c026075f532463b17c201202585925c3
justinwozniak <wozzytheprogrammer@gmail.com>
2021-05-26 15:44:35 -0400

    Built main component for scanner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import QrReader from 'react-qr-scanner';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class QrContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 'Hold QR code steady and clear to scan', 
+      error: ''
+    };
+    this.handleScan = this.handleScan.bind(this);
+  }
+
+  handleScan(result, data) {
+    this.setState({
+      result: data
+    });
+  }
+
+  handleError(err) {
+    console.log(err);
+  }
+  render() {
+    const previewStyle = {
+      height: 700,
+      width: 1000,
+      display: 'flex',
+      'justify-content': 'center'
+    };
+
+    const camStyle = {
+      display: 'flex',
+      'justify-content': 'center',
+      marginTop: '-50px'
+    };
+
+    const textStyle = {
+      fontSize: '30px',
+      'text-align': 'center',
+      marginTop: '-50px'
+    };
+
+    return (
+      <React.Fragment>
+        <div style={camStyle}>
+          <QrReader delay={100} style={previewStyle} onError={this.handleError} onScan={this.handleScan} />
+        </div>
+        <p>{this.state.error}</p>
+        <p style={textStyle}>{this.state.result}</p>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+export default QrContainer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import QrContainer from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QrContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
